1ef19915e21b889730bcca939e946e112409828e
Jon Jon <neumerance@live.com>
2021-11-30 11:34:42 +0800

    implement token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user_by_user_token
+
+  private
+
+  def authenticate_user_by_user_token
+    raise UnAuthorizedAccess unless current_user.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(token: token)
+  end
+
+  def token
+    token = request.headers['HTTP_AP_AUTH_TOKEN']
+
+    raise UnAuthorizedAccess unless token.present?
+
+    @token ||= token
+  end
+
+  class UnAuthorizedAccess < StandardError
+    def message
+      return I18n.t('exceptions.unauthorized_access')
+    end
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -2,12 +2,22 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  email      :string
-#  name       :string
-#  role       :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint           not null, primary key
+#  email                  :string
+#  encrypted_password     :string           default(""), not null
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  role                   :integer
+#  token                  :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class Client < User
   default_scope { where(role: :client) }
